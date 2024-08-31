4536ddc26f338b5da02ef00b760b4e69647f16dd
Kishor <krj946@gmail.com>
2024-08-31 14:21:36 +0530

    Update DB models

---
+++ src/main/java/org/crowdfund/models/ProjectCategory.java
@@ -0,0 +1,14 @@
+package org.crowdfund.models;
+
+/**
+ * enum class for Project categories
+ */
+public enum ProjectCategory {
+    MUSIC,
+    ART,
+    TECHNOLOGY,
+    FILM,
+    FASHION,
+    COMICS,
+    PHOTOGRAPHY
+}
